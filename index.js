4830791ca4a86b921a291079a7d175a6e5311ccb
Chris Parnin <chris.parnin@gmail.com>
2018-09-26 19:20:52 -0400

    Cleanup

--- index.js
+++ index.js
@@ -7,41 +7,43 @@ const yargs    = require('yargs');
 const yaml     = require('js-yaml');
 
 const Loader = require('./lib/inspect/loader');
+const BakerConnector = require('./lib/harness/baker');
 
 // Register run command
-yargs.command('grade <hws_path> <criteria_path>', 'Grade a repo that has grader.yml', (yargs) => {
+yargs.command('verify <baker_path> <criteria_path>', 'Verify an instance', (yargs) => {
 
-    yargs.positional('repo_url', {
-        describe: 'Repository URL',
-        type: 'string'
-    });
+    // yargs.positional('repo_url', {
+    //     describe: 'Repository URL',
+    //     type: 'string'
+    // });
 
 }, async (argv) => {
     // Get id and source directory
-    let hws_path = argv.hws_path;
+    let baker_path = argv.baker_path;
     let criteria_path = argv.criteria_path;
 
-    await main(hws_path, criteria_path);
+    await main(baker_path, criteria_path);
 });
 
 // Turn on help and access argv
 yargs.help().argv;
 
-async function main(hws_path, criteria_path)
+async function main(baker_path, criteria_path)
 {
-
+    await verify(baker_path, criteria_path);
 }
 
-async function verify(hw_path, criteria_path)
+async function verify(baker_path, criteria_path)
 {
+    let connector = new BakerConnector();
+
     let loader = new Loader();
     let checks = await loader.loadChecks(criteria_path);
 
     for( let check of checks )
     {
-        let instance = new check.module();
-        let context = { container: `${hw.id}-${check.host}`, 
-            host: check.host, hw_path: hw_path, hws_path: hws_path, hw: hw, autogradeYML: autogradeYML };
+        let instance = new check.module(connector);
+        let context = { bakerPath: baker_path };
         
         console.log(`checking ${check.name}`);
         let results = await instance.check( context, check.args );

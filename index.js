fb1b000093f95631ed4687653674d42d96014c5c
Samim Mirhosseini <samim.mirhoseini@gmail.com>
2018-10-13 13:59:11 -0400

    ssh connector POC #4

--- index.js
+++ index.js
@@ -10,24 +10,27 @@ const chalk    = require('chalk');
 const Loader = require('./lib/inspect/checks/loader');
 const Reporter = require('./lib/inspect/report');
 const BakerConnector = require('./lib/harness/baker');
+const SSHConnector = require('./lib/harness/ssh');
 
 // Register run command
-yargs.command('verify [baker_path] [criteria_path]', 'Verify an instance', (yargs) => {
+yargs.command('verify [env_address] [criteria_path]', 'Verify an instance', (yargs) => {
 
-    // yargs.positional('repo_url', {
-    //     describe: 'Repository URL',
-    //     type: 'string'
-    // });
+    yargs.positional('ssh_key', {
+        describe: 'required when using ssh connector. give path to private ssh key',
+        type: 'string',
+        alias: 'i'
+    });
 
 }, async (argv) => {
     // Get id and source directory
-    let baker_path = argv.baker_path ? argv.baker_path : process.cwd();
+    let connector_type = argv.ssh_key ? 'ssh' : 'baker';
+    let env_address = argv.env_address ? argv.env_address : process.cwd();
     let criteria_path = argv.criteria_path;
 
     // Default to baker_path
-    if( !criteria_path )
-    {
-        criteria_path = path.join(baker_path, 'test', 'opunit.yml');
+    if (!criteria_path) {
+        // TODO: this needs cleanup, trying to get it to work...
+        criteria_path = path.join(argv.ssh_key ? process.cwd() : env_address, 'test', 'opunit.yml');
         if( !fs.existsSync(criteria_path) )
         {
             console.error(chalk.red('Checks file was not provided, nor was default path found in test/opunit.yml'));
@@ -35,20 +38,24 @@ yargs.command('verify [baker_path] [criteria_path]', 'Verify an instance', (yarg
         }
     }
 
-    await main(baker_path, criteria_path);
+    await main(env_address, criteria_path, connector_type, argv.ssh_key);
 });
 
 // Turn on help and access argv
 yargs.help().argv;
 
-async function main(baker_path, criteria_path)
+async function main(env_address, criteria_path, connector_type, ssh_key)
 {
-    await verify(baker_path, criteria_path);
+    await verify(env_address, criteria_path, connector_type, ssh_key);
 }
 
-async function verify(baker_path, criteria_path)
+async function verify(env_address, criteria_path, connector_type, ssh_key)
 {
-    let connector = new BakerConnector();
+    let connector = null;
+    if (connector_type === 'ssh')
+        connector = new SSHConnector(env_address, ssh_key);
+    else if (connector_type === 'baker')
+        connector = new BakerConnector();
     let reporter  = new Reporter();
 
     let loader = new Loader();
@@ -61,7 +68,7 @@ async function verify(baker_path, criteria_path)
     for( let check of checks )
     {
         let instance = new check.module(connector, reporter);
-        let context = { bakerPath: baker_path };
+        let context = { bakerPath: env_address };
         
         console.log(chalk`\t{bold ${check.name} check}`);
         let results = await instance.check( context, check.args );

d675b6e5ab521c224c24d2a3bfb0d2923d5aff70
ratrevorrow <ratrevorrow@my.waketech.edu>
2018-10-30 14:57:31 -0400

    Issue #9: implemented vagrant and command usage in index.js

--- index.js
+++ index.js
@@ -1,91 +1,88 @@
 #!/usr/bin/env node
-const _        = require('lodash');
-const fs       = require('fs-extra');
-const path     = require('path');
-const child_process = require('child_process');
-const yargs    = require('yargs');
-const yaml     = require('js-yaml');
-const chalk    = require('chalk');
 
-const Loader = require('./lib/inspect/checks/loader');
-const Reporter = require('./lib/inspect/report');
-const BakerConnector = require('./lib/harness/baker');
-const SSHConnector = require('./lib/harness/ssh');
-const DockerConnector = require('./lib/harness/docker');
+const _ = require( 'lodash' );
+const fs = require( 'fs-extra' );
+const path = require( 'path' );
+const child_process = require( 'child_process' );
+const yargs = require( 'yargs' );
+const yaml = require( 'js-yaml' );
+const chalk = require( 'chalk' );
+
+const Loader = require( './lib/inspect/checks/loader' );
+const Reporter = require( './lib/inspect/report' );
+const BakerConnector = require( './lib/harness/baker' );
+const SSHConnector = require( './lib/harness/ssh' );
+const VagrantConnector = require( './lib/harness/vagrant' );
 
 // Register run command
-yargs.command('verify [env_address]', 'Verify an instance', (yargs) => {
-    yargs.positional('criteria_path', {
-        describe: 'path to the opunit.yml file, default is cwd/test/opunit.yml',
-        type: 'string',
-        alias: 'c'
-    });
+yargs.command( 'verify [env_address] [criteria_path]', 'Verify an instance', ( yargs ) => {
 
-    yargs.positional('ssh_key', {
+    yargs.positional( 'ssh_key', {
         describe: 'required when using ssh connector. give path to private ssh key',
         type: 'string',
         alias: 'i'
-    });
+    } );
 
-    yargs.positional('container', {
-        describe: 'required when using docker connector. give name or id of your container',
-        type: 'string',
-        alias: 't'
-    });
-}, async (argv) => {
+}, async ( argv ) => {
     // Get id and source directory
     let connector_type = argv.ssh_key ? 'ssh' : argv.container ? 'docker' : 'baker';
+    connector_type = argv.vagrant ? 'vagrant' : connector_type;
     let env_address = argv.container || argv.env_address || process.cwd();
     let criteria_path = argv.criteria_path;
+    let vm = argv.vagrant;
+    let path_to_opunit_yaml = argv.c ? argv.c : `${process.cwd()}/test/opunit.yml`;
 
     // Default to baker_path
-    if (!criteria_path) {
+    if ( !criteria_path ) {
         // TODO: this needs cleanup, trying to get it to work...
-        criteria_path = path.join(argv.ssh_key || argv.docker ? process.cwd() : env_address, 'test', 'opunit.yml');
-        if( !fs.existsSync(criteria_path) )
-        {
-            console.error(chalk.red('Checks file was not provided, nor was default path found in test/opunit.yml'));
-            process.exit(1);
+        criteria_path = path.join( argv.ssh_key ? process.cwd() : env_address, 'test', 'opunit.yml' );
+        if ( !fs.existsSync( criteria_path ) ) {
+            console.error( chalk.red( 'Checks file was not provided, nor was default path found in test/opunit.yml' ) );
+            process.exit( 1 );
         }
     }
 
-    await main(env_address, criteria_path, connector_type, argv.ssh_key);
-});
+    await main( env_address, criteria_path, connector_type, argv.ssh_key, vm, path_to_opunit_yaml );
+} );
 
 // Turn on help and access argv
 yargs.help().argv;
 
-async function main(env_address, criteria_path, connector_type, ssh_key)
-{
-    await verify(env_address, criteria_path, connector_type, ssh_key);
+async function main( env_address, criteria_path, connector_type, ssh_key, vm, path_to_opunit_yaml ) {
+    await verify( env_address, criteria_path, connector_type, ssh_key, vm, path_to_opunit_yaml );
 }
 
-async function verify(env_address, criteria_path, connector_type, ssh_key)
-{
+async function verify( env_address, criteria_path, connector_type, ssh_key, vm, path_to_opunit_yaml ) {
     let connector = null;
-    if (connector_type === 'ssh')
-        connector = new SSHConnector(env_address, ssh_key);
-    else if (connector_type === 'baker')
+    if ( connector_type === 'ssh' )
+        connector = new SSHConnector( env_address, ssh_key );
+    else if ( connector_type === 'baker' )
         connector = new BakerConnector();
-    else if (connector_type === 'docker')
-        connector = new DockerConnector();
-    let reporter  = new Reporter();
+    else if ( connector_type === 'vagrant' )
+        connector = new VagrantConnector();
+    let reporter = new Reporter();
 
-    await connector.ready();
+    try {
+        await connector.ready();
+    } catch ( err ) {
+        console.log( "Host is not running" );
+        return;
+    } 
 
     let loader = new Loader();
-    let checks = await loader.loadChecks(criteria_path);
+    let checks = await loader.loadChecks( criteria_path );
 
     console.log();
-    console.log(chalk.underline('Checks'))
+    console.log( chalk.underline( 'Checks' ) )
     console.log()
 
-    for( let check of checks )
-    {
-        let instance = new check.module(connector, reporter);
-        let context = { bakerPath: env_address };
-        
-        console.log(chalk`\t{bold ${check.name} check}`);
+    for ( let check of checks ) {
+        let instance = new check.module( connector, reporter );
+        let context = {
+            bakerPath: env_address
+        };
+
+        console.log( chalk `\t{bold ${check.name} check}` );
         let results = await instance.check( context, check.args );
         instance.report( results );
     }

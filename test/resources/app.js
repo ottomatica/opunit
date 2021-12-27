d17bdedb288c2517751bcf604114e523085ddf24
Chris Parnin <chris.parnin@gmail.com>
2021-12-26 19:46:46 -0500

    basic passing test.

---
+++ test/resources/app.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const requestListener = function (req, res) {
+  res.writeHead(200);
+  res.end('Hello, World!');
+}
+
+const server = http.createServer(requestListener);
+server.listen(8080);
+
+console.log("Started Application");
